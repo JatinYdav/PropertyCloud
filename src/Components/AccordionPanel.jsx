f3d2ce04b2c105e8a211c7a6969f223c49bf1f95
JatinYdav <ydvjatin30@gmail.com>
2024-01-06 17:02:37 +0530

    Re-made the Non-working Accordion

--- src/Components/AccordionPanel.jsx
+++ src/Components/AccordionPanel.jsx
@@ -1,38 +1,79 @@
 import React from "react";
-import '../Styles/AccordionPanel.css'
-import Accordion from "./SubComponents/Accordion";
+import "../Styles/AccordionPanel.css";
+import ArrowDownCircle from "../Svgs/SmallIcons/ArrowDownCircle"
 import ACPicon1 from "../Svgs/SmallIcons/ACPicon1";
 import ACPicon4 from "../Svgs/SmallIcons/ACPicon4";
 const AccordionPanel = () => {
   return (
     <div className="accordionpanel">
-      <Accordion
-        className="ac1"
-        acicon={<ACPicon1 />}
-        heading="Exquisite Properties"
-        accordiondescription="We pride ourselves on delivering unparalleled real estate experiences,
-              connecting you with the most luxurious and sought-after homes. With
-              our expert team and extensive network, we ensure that your dream
-              property becomes a reality."
-      />
-      <Accordion
-        className="ac2"
-        acicon={<ACPicon4 />}
-        heading="Best Prices in the Market"
-        accordiondescription="We not only bring you the best properties in town, but also ensure that you secure them at the most competitive prices in the market. Our dedicated team tirelessly negotiates to get you the best deals, maximizing the value of your investment."
-      />
-      <Accordion
-        className="ac1"
-        acicon={<ACPicon4 />}
-        heading="Industrial Deals "
-        accordiondescription="We specialize in industrial land deals, catering to the needs of businesses &  investors alike.Our expert team understands the unique requirements of industrial ventures, and take pride in connecting you with prime locations that fuel growth and success."
-      />
-      <Accordion
-        className="ac1"
-        acicon={<ACPicon4 />}
-        heading="Sell & Buy"
-        accordiondescription="Whether you're looking to buy your dream property or sell your current one, we got you covered. Our dedicated team uses innovative marketing strategies & leverages our extensive network to ensure a seamless selling experience for your property."
-      />
+      <div className="accordion ac1">
+        <div className="accordion-content">
+          <h1>
+            <div className="subbox">
+              <ACPicon1 />
+              <span>Exquisite Properties</span>
+            </div>
+            <ArrowDownCircle />
+          </h1>
+          <p className="accordiondescription">
+            We pride ourselves on delivering unparalleled real estate
+            experiences,connecting you with the most luxurious and sought-after
+            homes. With our expert team and extensive network, we ensure that
+            your dream property becomes a reality.
+          </p>
+        </div>
+      </div>
+      <div className="accordion ac2">
+        <div className="accordion-content">
+          <h1>
+            <div className="subbox">
+              <ACPicon4 />
+              <span>Best Prices in the Market</span>
+            </div>
+            <ArrowDownCircle />
+          </h1>
+          <p className="accordiondescription">
+            We not only bring you the best properties in town, but also ensure
+            that you secure them at the most competitive prices in the market.
+            Our dedicated team tirelessly negotiates to get you the best deals,
+            maximizing the value of your investment.
+          </p>
+        </div>
+      </div>
+      <div className="accordion ac3">
+        <div className="accordion-content">
+          <h1>
+            <div className="subbox">
+              <ACPicon4 />
+              <span>Industrial Deals</span>
+            </div>
+            <ArrowDownCircle />
+          </h1>
+          <p className="accordiondescription">
+            We pride ourselves on delivering unparalleled real estate
+            experiences, connecting you with the most luxurious and sought-after
+            homes. With our expert team and extensive network, we ensure that
+            your dream property becomes a reality.
+          </p>
+        </div>
+      </div>
+      <div className="accordion ac4">
+        <div className="accordion-content">
+          <h1>
+            <div className="subbox">
+              <ACPicon4 />
+              <span>Sell & Buy</span>
+            </div>
+            <ArrowDownCircle />
+          </h1>
+          <p className="accordiondescription">
+            Whether you're looking to buy your dream property or sell your
+            current one, we got you covered. Our dedicated team uses innovative
+            marketing strategies & leverages our extensive network to ensure a
+            seamless selling experience for your property.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
