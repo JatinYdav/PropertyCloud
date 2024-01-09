21053f9211f778cd6d3d60f8cbb4f641f27bda99
JatinYdav <ydvjatin30@gmail.com>
2024-01-09 18:27:06 +0530

    Fixed the Residence Image Counter

--- src/Components/PopularResidences.jsx
+++ src/Components/PopularResidences.jsx
@@ -1,9 +1,7 @@
 import React from 'react'
 import ResidenceCard from './SubComponents/ResidenceCard'
 import '../Styles/PopularResidences.css'
-import Residence1 from '../Svgs/Residence1'
-import Residence2 from '../Svgs/Residence2'
-import Residence3 from '../Svgs/Residence3'
+
 
 const PopularResidences = () => {
   return (
@@ -14,19 +12,19 @@ const PopularResidences = () => {
       </div>
       <div className="cardbox">
         <ResidenceCard className="card1"
-          display={<Residence1 />}
+          display={1}
           location="Japanese Zone, Neemrana"
           name=" Ashiana Green Hills"
           description="Some small content here that spans two lines and is nice blah blah blah blah blah"
         />
         <ResidenceCard className="card2"
-          display={<Residence2/>}
+          display={2}
           location="Japanese Zone, Neemrana"
           name="Eldeco Eden Park"
           description="Offering you a bouquet of options from high-end independent villas to 2 & 3 bedroom Low and High-rise apartments."
         />
         <ResidenceCard className="card3"
-          display={<Residence3/>}
+          display={3}
           location="Japanese Zone, Neemrana"
           name="Ashadeep Green Acres"
           description="Some small content here that spans two lines and is nice blah blah blah blah blah"
--- src/Components/SubComponents/ResidenceCard.jsx
+++ src/Components/SubComponents/ResidenceCard.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import MapPin from '../../Svgs/MapPin'
 import '../../Styles/SubComponentStyles/ResidenceCard.css'
-import Residence1 from '../../Svgs/Residence/Residence1Photo (4).svg'
+import Residence1 from "../../Svgs/Residence/Residence1.svg"
+import Residence2 from "../../Svgs/Residence/Residence2.svg";
+import Residence3 from "../../Svgs/Residence/Residence3.svg";
 const ResidenceCard = (props) => {
+  
   return (
     <div className="{props.className} card">
       <div className="cardimg">
-        <img src={Residence1} alt="r1" className='cardimgg'/>
+        <img src={(props.display===1)?Residence1:(props.display===2)?Residence2:Residence3} alt="r1" className='cardimgg'/>
       </div>
       <div className="cardinfo">
         <h2 className="rlocation">
