4a402e11272fedacff8f0af4c32eed2232b8997e
JatinYdav <ydvjatin30@gmail.com>
2024-01-10 20:23:54 +0530

    Optimisation for mobile Starting

--- src/Components/ContactSection.jsx
+++ src/Components/ContactSection.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import "../Styles/ContactSection.css";
-import ContactSectionImg from '../Svgs/BackgroundSvgs/3 1.svg'
 const ContactSection = () => {
   return (
     <div className="contactsection">
         <div className="contactimgcontainer">
-          
+          <div className="subcontainer1"></div>
+          <div className="subcontainer2"></div>
+          <div className="subcontainer3"></div>
+        </div>
+        <div className="contactheading">
+          <div className="headingsubcontainer">
+            <h1 className="heading1">Contact</h1>
+            <h1 className="heading2">Us</h1>
+          </div>
         </div>
     
     </div>
