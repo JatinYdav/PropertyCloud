8dafdb6ae75adccdba8022735eebb7368f603243
JatinYdav <ydvjatin30@gmail.com>
2024-01-12 21:25:26 +0530

    Skeleton of ContactCard

--- src/Components/ContactSection.jsx
+++ src/Components/ContactSection.jsx
@@ -3,18 +3,26 @@ import "../Styles/ContactSection.css";
 const ContactSection = () => {
   return (
     <div className="contactsection">
-        <div className="contactimgcontainer">
-          <div className="subcontainer1"></div>
-          <div className="subcontainer2"></div>
-          <div className="subcontainer3"></div>
-        </div>
-        <div className="contactheading">
-          <div className="headingsubcontainer">
-            <h1 className="heading1">Contact</h1>
-            <h1 className="heading2">Us</h1>
+      <div className="contactimgcontainer">
+        <div className="contactcard">
+          <div className="contactcardinfo1">
+            <h2>PROPERTY CLOUD BY</h2>
+            <h1>DHIRAJ KUMAR</h1>
+          </div>
+          <div className="contactcardinfo2">
+            <h1>+91 978 540 0065</h1>
+          </div>
+          <div className="contactcardinfo3">
+            <h3>Made By @ydav_Jatin</h3>
           </div>
         </div>
-    
+      </div>
+      <div className="contactheading">
+        <div className="headingsubcontainer">
+          <h1 className="heading1">Contact</h1>
+          <h1 className="heading2">Us</h1>
+        </div>
+      </div>
     </div>
   );
 };
