01215d60a7cacbd93f748042125f91d7791e8430
JatinYdav <ydvjatin30@gmail.com>
2024-01-13 19:26:39 +0530

    Contact Card Final Stage PC version

--- src/Components/ContactSection.jsx
+++ src/Components/ContactSection.jsx
@@ -5,12 +5,14 @@ const ContactSection = () => {
     <div className="contactsection">
       <div className="contactimgcontainer">
         <div className="contactcard">
-          <div className="contactcardinfo1">
-            <h2>PROPERTY CLOUD BY</h2>
-            <h1>DHIRAJ KUMAR</h1>
-          </div>
-          <div className="contactcardinfo2">
-            <h1>+91 978 540 0065</h1>
+          <div className="inf12">
+            <div className="contactcardinfo1">
+              <h2>PROPERTY CLOUD BY</h2>
+              <h1>DHIRAJ KUMAR</h1>
+            </div>
+            <div className="contactcardinfo2">
+              <h1>+91 978 540 0065</h1>
+            </div>
           </div>
           <div className="contactcardinfo3">
             <h3>Made By @ydav_Jatin</h3>
